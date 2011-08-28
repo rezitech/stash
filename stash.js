ad91e1c23ed9c6f9d9dcaa764c4ee4253c72b456
Jonathan Neal <jonathantneal@hotmail.com>
2011-08-28 12:39:08 -0700

    Stash v1.2.2

--- stash.js
+++ stash.js
@@ -1,4 +1,4 @@
-/*! Stash v1.2.1 MIT/GPL2 @rezitech */
+/*! Stash v1.2.2 MIT/GPL2 @rezitech */
 (function (win, doc, ls) {
 	// Returns a safely quoted string
 	function quoteStr (str) {
@@ -108,13 +108,15 @@
 			var item = this.get(attr);
 			// if both the existing item and the new item are an array
 			if ( isType('Array', item) && isType('Array', val) ) val = item.concat(val);
-			// if both the existing item is an array
+			// if the existing item is an array
 			else if ( isType('Array', item) ) val = item.push(val) && item;
-			// if both the existing item and the new item are a boolean
+			// if the existing item is a boolean
 			else if ( isType('Boolean', item) ) val = item && !!val;
-			// if both the existing item and the new item are a boolean
+			// if the existing item is a date and the new item can be a number
 			else if ( isType('Date', item) && !isNaN(val * 1) ) val = new Date(item.getTime() + (val * 1));
-			// if both the existing item and the new item are a number
+			// if both the existing item and the new item are a function
+			else if ( isType('Function', item) && isType('Function', val) ) val = new Function('return('+String(item)+').apply(this, arguments)&&('+String(val)+').apply(this, arguments)');
+			// if the existing item is a number and the new item can be a number
 			else if ( isType('Number', item) && !isNaN(val * 1) ) val = item + (val * 1);
 			// if both the existing item and the new item are an object
 			else if ( isType('Object', item) && isType('Object', val) ) val = extendObject(item, val);
@@ -126,7 +128,7 @@
 				regExpB = String(val).match(regExpMatch);
 				val = new RegExp(regExpA[1]+regExpB[1], regExpA[2]+regExpB[2]);
 			}
-			// if both the existing item and the new item are a string
+			// if the existing item is a string
 			else if ( isType('String', item) ) val = item+val;
 			// set the new item and return its value
 			return this.set(attr, val);
